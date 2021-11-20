f1065bf85d9cdbed559c410919c00b4259c42c33
chatblack1999 <chatblack1999@gmail.com>
2021-11-20 14:28:44 -0600

    [FIXED] show on left side in desktop

--- src/components/global/Header.jsx
+++ src/components/global/Header.jsx
@@ -9,7 +9,7 @@ const Header = () => {
       <div className="w-full shadow-xl">
         <div className="w-full lg:w-10/12 mx-auto flex flex-row justify-between">
           {/* <div className="p-2"> */}
-          <img className="w-2/3 md:w-1/3 lg:w-2/12 my-2 mx-auto lg:mr-auto" src={BrandLogo} alt="" srcSet="" />
+          <img className="w-2/3 md:w-1/3 lg:w-2/12 my-2 mx-auto lg:mx-0 lg:mr-auto" src={BrandLogo} alt="" srcSet="" />
           {/* </div> */}
           <Menu
             menuFlag={menuFlag}
