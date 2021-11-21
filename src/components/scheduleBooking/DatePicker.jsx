25a35318f9d1647fcd58b2465744fbd4cce4fbc1
chatblack1999 <chatblack1999@gmail.com>
2021-11-21 16:43:47 -0600

    [ADDED] more options on schedule booking

--- src/components/scheduleBooking/DatePicker.jsx
+++ src/components/scheduleBooking/DatePicker.jsx
@@ -14,14 +14,37 @@ const DatePicker = (props) => {
 
   return (
     <>
+      <label className="block mx-auto mb-2">
+        <span className="text-white  font-semibold">Fecha</span>
+      </label>
       <input
-        className="bg-white text-black border-2 rounded-lg p-2 w-2/3 lg:w-1/2 mx-auto"
+        className="bg-white text-black border-2 rounded-lg p-2 w-2/3 lg:w-1/2 mx-auto my-1"
         type="date"
         id="start"
         name="trip-start"
         value={date}
         onChange={(e) => setDate(e.target.value)}
       />
+      <div className="flex flex-col my-1">
+        <label
+          htmlFor="booking_time"
+          className="text-white mx-auto mb-2 font-semibold"
+        >
+          Elige un horario
+        </label>
+        <input
+          id="booking_time"
+          name="booking_time"
+          type="time"
+          step="600"
+          required
+          placeholder="hh-mm"
+          // value={values.booking_time}
+          // onChange={handleChange}
+          // onBlur={handleBlur}
+          className="bg-white text-black border-2 rounded-lg p-2 w-2/3 lg:w-1/2 mx-auto"
+        />
+      </div>
     </>
   );
 };
--- src/components/scheduleBooking/Step1.jsx
+++ src/components/scheduleBooking/Step1.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { DatePicker } from "./";
 
 const Step1 = (props) => {
-  const {setDate, date, setNotEmty} = props;
+  const { setDate, date, setNotEmty } = props;
   return (
     <>
       <div className="mt-auto flex flex-col">
@@ -14,6 +14,37 @@ const Step1 = (props) => {
         </p>
         <div className="flex flex-col p-4">
           <DatePicker setDate={setDate} date={date} setNotEmty={setNotEmty} />
+          <div className="flex flex-col my-1">
+            <label className="block mx-auto mb-2">
+              <span className="text-white  font-semibold">
+                Motivo de consulta
+              </span>
+            </label>
+            <select
+              name="reason"
+              className="bg-white text-black border-2 rounded-lg p-2 w-2/3 lg:w-1/2 mx-auto"
+              // value={values.reason}
+              // onChange={handleChange}
+              // onBlur={handleBlur}
+            >
+              <option>Seleccione</option>
+              {/* {loading ? (
+                  <option>Cargando...</option>
+                ) : (
+                  topics.map((topic, index) => (
+                    <option key={index} value={topic.id}>
+                      {topic.name}
+                    </option>
+                  ))
+                )} */}
+            </select>
+            {/* <ErrorMessage
+                name="reason"
+                component="div"
+                className="italic font-bold font-sans text-red-500"
+              /> */}
+          </div>
+
         </div>
       </div>
     </>
