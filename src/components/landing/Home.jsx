5fd698810a1d167596a2b2ba5059cea1013972b1
chatblack1999 <chatblack1999@gmail.com>
2021-11-21 13:42:11 -0600

    [FIXED] hero section center aling

--- src/components/landing/Home.jsx
+++ src/components/landing/Home.jsx
@@ -7,29 +7,39 @@ import Header from "../global/Header";
 const Home = () => {
   return (
     <>
-      <Header />
-      <div className="flex flex-col lg:flex-row w-10/12 mx-auto h-full">
-        <div className="h-1/2 my-auto">
-          <img className="mx-auto h-1/2 my-auto" src={Hero} alt="" srcSet="" />
-        </div>
-        <div className="flex flex-col justify-center my-auto">
-          <div className="flex flex-row mx-auto mb-6">
-            <h1 className="text-center text-4xl font-bold text-blue-regular mr-6">
-              ¡Bienvenido a Pracofi!
-            </h1>
-           <img className="" src={Logo} alt="" srcSet="" />
+      <div className="lg:h-screen flex flex-col">
+        <Header />
+        <div className="flex flex-col lg:flex-row w-10/12 mx-auto h-1/2 my-4 lg:my-auto">
+          <div className="h-full m-auto">
+            <img
+              className="h-full"
+              src={Hero}
+              alt=""
+              srcSet=""
+            />
           </div>
+          <div className="flex flex-col justify-center m-auto">
+            <div className="flex flex-row mx-auto mb-6">
+              <h1 className="text-center text-4xl font-bold text-blue-regular mr-6">
+                ¡Bienvenido a Pracofi!
+              </h1>
+              <img className="" src={Logo} alt="" srcSet="" />
+            </div>
 
-          <span className="text-center uppercase text-2xl text-blue-regular mb-2">
-            Asesoria contable fiscal
-          </span>
-          <p className="w-10/12 mx-auto text-lg text-blue-regular mb-4">
-         ¿Como podemos ayudarte? en Pracofi nos preocupamos por ti.
-         ¡Agenda tu cita y comencemos ahora!
-          </p>
-          <Link to="/quote" className="w-3/4 mx-auto uppercase p-6 bg-black hover:bg-blue-dark text-white text-center">
-            Agendar una cita
-          </Link>
+            <span className="text-center uppercase text-2xl text-blue-regular mb-2">
+              Asesoria contable fiscal
+            </span>
+            <p className="w-10/12 mx-auto text-lg text-blue-regular mb-4">
+              ¿Como podemos ayudarte? en Pracofi nos preocupamos por ti. ¡Agenda
+              tu cita y comencemos ahora!
+            </p>
+            <Link
+              to="/quote"
+              className="w-3/4 mx-auto uppercase p-6 bg-black hover:bg-blue-dark text-white text-center"
+            >
+              Agendar una cita
+            </Link>
+          </div>
         </div>
       </div>
     </>
