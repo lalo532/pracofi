16eb5a9888274a382d655d3264ace41978cc40f1
chatblack1999 <chatblack1999@gmail.com>
2021-11-23 22:46:57 -0600

    [REMOVED] set port on start

--- src/services/Bookings.js
+++ src/services/Bookings.js
@@ -4,3 +4,28 @@ export const getBookings = () => {
     const response = axios.get("http://localhost:3000/register", config);
     return response;
 }
+
+export const RegisterBooking = (nombre, email, password) => {
+  // const history = useHistory();
+  return axios
+    .post("http://localhost:3000/register", {
+      nombre,
+      email,
+      password,
+    })
+    .then((res) => {
+      //   console.log(res);
+      // const name = res.data.usuario.name;
+      // const role = res.data.usuario.role;
+      // const name = res.data.usuario.name;
+      // const role = res.data.usuario.role;
+      // console.log(res.data.token);
+      //   if (res.status === 200) {
+      //     history.push("/login");
+      //   }
+      return res.data;
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+};
