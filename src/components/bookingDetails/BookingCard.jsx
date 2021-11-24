fb4d18ad22bee916f88b680dc52edce17562278b
chatblack1999 <chatblack1999@gmail.com>
2021-11-24 02:22:48 -0600

    [ADDED] booking details strctre

---
+++ src/components/bookingDetails/BookingCard.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const BookingCard = (props) => {
+  return (
+    <>
+      <div className="w-10/12 lg:w-1/2 flex flex-col m-auto bg-gray-200">
+        <div>Lorem ipsum dolor sit amet.</div>
+        <div>Lorem ipsum dolor sit amet.</div>
+        <div>Lorem ipsum dolor sit amet.</div>
+        <div>Lorem ipsum dolor sit amet.</div>
+      </div>
+    </>
+  );
+};
+
+export default BookingCard;
--- src/pages/BookingDetail.jsx
+++ src/pages/BookingDetail.jsx
@@ -1,11 +1,22 @@
-import React from 'react'
+import React from "react";
+import { BookingCard } from "../components/bookingDetails/BookingCard";
+import { HeaderUser } from "../components/global/";
 
 const BookingDetail = () => {
-    return (
-        <>
-            BookingDetail works!
-        </>
-    )
-}
+  return (
+    <>
+      <div className="w-full h-full flex flex-col">
+        {/* <BookingCard /> */}
+        <HeaderUser />
+        <div className="w-10/12 lg:w-1/2 flex flex-col m-auto bg-gray-200">
+          <div>Lorem ipsum dolor sit amet.</div>
+          <div>Lorem ipsum dolor sit amet.</div>
+          <div>Lorem ipsum dolor sit amet.</div>
+          <div>Lorem ipsum dolor sit amet.</div>
+        </div>
+      </div>
+    </>
+  );
+};
 
-export default BookingDetail
+export default BookingDetail;
